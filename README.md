f9493bab8fd8e1e0d306410556faaf2faac02ec9
Pierre-Etienne Bougué <pierre-etienne.bougue@kisio.com>
2021-03-31 18:47:46 +0200

    Revert "[tech] Revert #21 & #20, so remove 'proj-ci' for now"

--- README.md
+++ README.md
@@ -7,17 +7,58 @@ This repository contains helpful Docker images for the CI with CanalTP's project
 A few Docker images are available containing most of the useful tools to build
 and verify projects.
 
+### `kisiodigital/proj-ci:7.2.1-artifacts`
+
+This Docker image provides pre-built artifacts for [proj] library,
+based on a Debian stretch.
+
+In order to use it in a multistage Docker image, you can do the following setup.
+
+```
+FROM kisiodigital/proj-ci:7.2.1-artifacts as proj-artifacts
+
+FROM debian:stretch
+
+COPY --from=proj-artifacts /proj-artifacts /
+ENV RUNTIME_DEPENDENCIES="clang libtiff5 libcurl3-nss"
+RUN apt update \
+    && apt install --yes ${RUNTIME_DEPENDENCIES} \
+    && apt autoremove --yes \
+    && rm -rf /var/lib/apt/lists/*
+```
+
+Do not forget to install the runtime dependencies on which the [proj] artifacts depend.
+
+### `kisiodigital/proj-ci:7.2.1`
+
+This Docker image provides all the binaries and libraries of the project [proj],
+based on a Debian stretch.
+
 ### `kisiodigital/rust-ci:latest`
 
-Based on Docker image `rust`, it provides the additional tools:
+Based on Docker image Debian stretch, it provides the additional tools:
 
+- [`rustup`]: setup for a functional Rust ecosystem
 - [`rustfmt`]: auto-formatting of the Rust source code
 - [`clippy`]: static linting of the Rust source code
+- [`cargo-audit`]: cargo tool to report known CVE in the dependencies
+- [`docker`]: docker engine
 
+If you want to release this image with the latest version of Rust, just trigger the Jenkins job manually.
+
+To know which version of Rust is embedded in the image, do the following:
+
+```sh
+docker run -rm kisiodigital/rust-ci:latest rustc --version
+```
+
+[`rustup`]: https://rustup.rs/
 [`rustfmt`]: https://github.com/rust-lang/rustfmt
 [`clippy`]: https://github.com/rust-lang/rust-clippy
+[`cargo-audit`]: https://github.com/RustSec/cargo-audit
+[`docker`]: https://www.docker.com/
 
-#### Variant `kisiodigital/rust-ci:latest-proj`
+#### Variant `kisiodigital/rust-ci:latest-proj7.2.1`
 
 Based on Docker image `kisiodigital/rust-ci:latest`, it provides also the
 [Proj] library, allowing to compile a Rust project with [`proj`]'s crate.
--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 HELP="./release.sh <image> [<variant>]
+    -d | --dry-run         build but do not push docker images to the registry
+    -p | --proj-version    version of proj dependency
+    -h | --help            display this help message
+    -t | --tag             specify the tag of the Docker image (default: 'latest' or 'latest-variant')
+    -s | --stage           specify an intermediate Docker stage to release (default: to latest stage)
 
 This script builds and pushes a Docker image named \`<image>-ci:latest\` using \`./<image>/Dockerfile\`.
 
@@ -16,10 +21,16 @@ DOCKER_NAMESPACE='kisiodigital'
 DRY_RUN='false'
 while [[ ${#} -gt 0 ]]; do
     case "${1}" in
+    '-t' | '--tag') shift 1;
+        IMAGE_TAG="${1}" ;;
+    '-s' | '--stage') shift 1 ;
+        STAGE="${1}" ;;
     '-d' | '--dry-run') DRY_RUN='true' ;;
-	'-h' | '--help') echo "${HELP}" && exit 0 ;;
+    '-p' | '--proj-version') shift 1;
+        PROJ_VERSION="${1}" ;;
+    '-h' | '--help') echo "${HELP}" && exit 0 ;;
     *) if [[ "${1}" =~ ^- ]]; then
-           Error "flag ${1} is unknown, the only recognized flag is '--dry-run (-d)'"
+           Error "flag ${1} is unknown"
        else
            if [[ -z "${IMAGE}" ]]; then
                IMAGE="${1}"
@@ -42,21 +53,33 @@ fi
 # [ -n "$(git status --untracked-files=no --porcelain)" ] && Error "git status is not clean, build aborted"
 
 # step 0: prepare docker image name with registry and tag
-# get the tag from git
-TAG=$(git describe --tags --abbrev=0 2> /dev/null)
-HAS_TAG=$(git tag --list --points-at HEAD)
-[ -z "${HAS_TAG}" ] && TAG='latest'
-[ -z "${TAG}" ] && Error "impossible to get a tag"
+# get the tag from git, only if not already specified (see '--tag' option)
+[ -z "${ARG_TAG}" ] && ARG_TAG=$(git describe --tags --abbrev=0 2> /dev/null)
+[ -z "${ARG_TAG}" ] && ARG_TAG=latest
 
 # step 1: build docker images for git HEAD
 if [[ -z "${IMAGE_VARIANT}" ]]; then
-	DOCKERFILE_PATH="./${IMAGE}/"
-    IMAGE_FULLNAME="${DOCKER_NAMESPACE}/${IMAGE}-ci:${TAG}"
+    DOCKERFILE_PATH="./${IMAGE}/"
+    if [[ -z "${IMAGE_TAG}" ]]; then
+        IMAGE_FULLNAME="${DOCKER_NAMESPACE}/${IMAGE}-ci:${ARG_TAG}"
+    else
+        IMAGE_FULLNAME="${DOCKER_NAMESPACE}/${IMAGE}-ci:${IMAGE_TAG}"
+    fi
     BUILD_ARG=''
 else
-	DOCKERFILE_PATH="./${IMAGE}/${IMAGE_VARIANT}"
-    IMAGE_FULLNAME="${DOCKER_NAMESPACE}/${IMAGE}-ci:${TAG}-${IMAGE_VARIANT}"
-    BUILD_ARG="--build-arg TAG=${TAG}"
+    DOCKERFILE_PATH="./${IMAGE}/${IMAGE_VARIANT}"
+    if [[ -z "${IMAGE_TAG}" ]]; then
+        IMAGE_FULLNAME="${DOCKER_NAMESPACE}/${IMAGE}-ci:${ARG_TAG}-${IMAGE_VARIANT}"
+    else
+        IMAGE_FULLNAME="${DOCKER_NAMESPACE}/${IMAGE}-ci:${IMAGE_TAG}"
+    fi
+    BUILD_ARG="--build-arg TAG=${ARG_TAG}"
+fi
+if [[ ! -z "${STAGE}" ]]; then
+    BUILD_ARG="${BUILD_ARG} --target ${STAGE}"
+fi
+if [[ ! -z "${PROJ_VERSION}" ]]; then
+    BUILD_ARG="${BUILD_ARG} --build-arg ${PROJ_VERSION}"
 fi
 echo "Building $IMAGE_FULLNAME"
 docker build --pull --no-cache --force-rm ${BUILD_ARG} --tag "${IMAGE_FULLNAME}" "${DOCKERFILE_PATH}"
